c16d628d3c9da3d5706ff25b2f52deac3f461367
Daniel Bitka <daniel.bitka@gmail.com>
2019-10-05 15:48:58 +0200

    clean code project

--- src/main/java/com/example/school/repository/RoleRepository.java
+++ src/main/java/com/example/school/repository/RoleRepository.java
@@ -4,10 +4,13 @@ import com.example.school.entity.Role;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Set;
+
 
 @Repository
 public interface RoleRepository extends JpaRepository<Role, Long> {
 
 
+    Set<Role> findAllById (Set<Long> id);
 
 }
